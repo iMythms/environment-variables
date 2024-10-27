ba8e5c8ad421af3b18063e7ed1c6192c1ca5145a
iMythms <maythams.149@gmail.com>
2024-10-27 18:23:23 +0300

    Refactor server.js to use dynamic port and log the listening address

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config()
 const express = require('express')
 
 const app = express()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
 	res.send('The server is running!')
 })
 
 // adjust the following:
-app.listen(port, () => {
-	console.log('Listening on port 3000')
+app.listen(PORT, () => {
+	console.log(`Listening on localhost:${PORT}`)
 	console.log(`Your secret is ${process.env.SECRET_PASSWORD}`)
 })
